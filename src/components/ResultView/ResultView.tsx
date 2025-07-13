ae45f0e9a9d54d97ea1b5e695a4cbaee9cec4ab4
Mikalai Shalupin <mikalai.shalupin@gmail.com>
2025-07-13 22:54:15 +0300

    feat: update description

--- src/components/ResultView/ResultView.tsx
+++ src/components/ResultView/ResultView.tsx
@@ -6,13 +6,26 @@ interface ResultViewProps {
 
 interface Person {
   uid: string;
-  properties: { name: string; height: number };
+  properties: PersonDescription;
+}
+
+interface PersonDescription {
+  name: string;
+  birth_year: string;
+  gender: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  eye_color: string;
 }
 
 export class ResultView extends React.Component<ResultViewProps> {
+  createDescription(info: PersonDescription) {
+    return `Year of birth: ${info.birth_year}\n Gender: ${info.gender}\n Height: ${info.height}\n Mass: ${info.mass}\n Hair color: ${info.hair_color}\n Eye color: ${info.eye_color}`;
+  }
+
   render() {
     const { data } = this.props;
-
     if (data.length === 0) {
       return <div>No result found</div>;
     }
@@ -29,7 +42,9 @@ export class ResultView extends React.Component<ResultViewProps> {
           {data.map((item: Person) => (
             <tr key={item.uid}>
               <td>{item.properties.name}</td>
-              <td>{item.properties.height}</td>
+              <td>
+                <pre>{this.createDescription(item.properties)}</pre>
+              </td>
             </tr>
           ))}
         </tbody>
