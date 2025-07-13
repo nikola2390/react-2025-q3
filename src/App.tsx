7b69b0128b9c5120620e75771de4de3e3889efc4
Mikalai Shalupin <mikalai.shalupin@gmail.com>
2025-07-13 19:33:42 +0300

    feat: basic search

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+type AppState = {
+  query: string;
+  response: string | null;
+};
+
+class App extends Component<object, AppState> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      query: '',
+      response: null,
+    };
+  }
+
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ query: event.target.value });
+  };
+
+  handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    fetch(`https://www.swapi.tech/api/people/?name=${this.state.query}`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          response: JSON.stringify(data.result),
+        });
+      })
+      .catch((err) => console.error(err));
+  };
+
   render(): React.ReactNode {
     return (
       <div className="content-wrapper">
         <div className="search-section">
-          <form>
-            <input type="text" placeholder="Search" className="search" />
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              placeholder="Search"
+              className="search"
+              onChange={this.handleInputChange}
+            />
             <button type="submit" className="search-btn">
               Search
             </button>
           </form>
         </div>
-        <div className="result"></div>
+        <div className="result">{this.state.response || 'Result is empty'}</div>
       </div>
     );
   }
